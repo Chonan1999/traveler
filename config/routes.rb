92587dea5e3b43cf66b022d7dee83fd22c66f33f
Chonan1999 <176509295+Chonan1999@users.noreply.github.com>
2024-09-10 23:47:27 +0900

    コメント機能実装　ビュー作成まで

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'posts/new' #削除
   post 'posts' => 'posts#create' #削除
-  resources :posts
+  resources :posts do
+    resources :comments, only:[:create, :destroy]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,6 +13,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get 'top' => 'homes#top' #この行を削除
-  root :to => 'homes#top'  #この行を記述
+  root :to => 'homes#top'
 end
