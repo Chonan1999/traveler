58cd79ea6b8ed0a8b7d9a5058d3d1a257dddb281
Chonan1999 <176509295+Chonan1999@users.noreply.github.com>
2024-09-15 23:30:15 +0900

    いいね機能実装　次ユーザ詳細

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,7 @@
 class FavoritesController < ApplicationController
     def create
         post = Post.find(params[:post_id])
-        favorited = current_user.favorites.new(post_id: post.id)
+        favorite = current_user.favorites.new(post_id: post.id)
         favorite.save
         redirect_to post_path(post)
     end
