e9f57687dae0dc9f57c23a4d55e056f74e35156d
Chonan1999 <176509295+Chonan1999@users.noreply.github.com>
2024-10-22 21:17:28 +0900

    コントローラ変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
 
     protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      
     end
 
     def after_sign_in_path_for(resource)
-      
+      Posts_path
     end
 
 end
