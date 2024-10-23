ff5753766257736fa14e189ed3ab61ddceea9719
Chonan1999 <176509295+Chonan1999@users.noreply.github.com>
2024-10-23 23:51:30 +0900

    コントローラ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   end
   
   def followers
-    user = User.find(params[:id])
+    
     @users = user.follower_user.page(params[:page]).per(3).reverse_order
   end
 
