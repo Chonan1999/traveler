cf0d890dacdefb5c368f5c9a918490b081f78d49
Chonan1999 <176509295+Chonan1999@users.noreply.github.com>
2024-10-26 20:02:38 +0900

    コントローラ修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
     def create
         post = Post.find(params[:post_id])
-        @comment = current_user.comments.new(comment_params)
+        
         @comment.post_id = post.id
         if @comment.save
             redirect_to post_path(post)
