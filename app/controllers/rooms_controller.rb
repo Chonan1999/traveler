1cca3e93b3c6f03803e0d5718b8c4f0883f6ba6c
Chonan1999 <176509295+Chonan1999@users.noreply.github.com>
2024-12-01 20:40:33 +0900

    DM機能追加完了

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,24 +1,25 @@
 class RoomsController < ApplicationController
 
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
     def create
-        @room = Room.create(user_id: current_user.id)
-        @entry1 = Entry.create(:room_id => @room.id, :user_id => current_user.id)
-        @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(:room_id => @room.id))
-        redirect_to "/rooms/#{@room.id}"
-      end
+      @room = Room.create(user_id: current_user.id)
+      @entry1 = Entry.create(:room_id => @room.id, :user_id => current_user.id)
+      @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(:room_id => @room.id))
+      redirect_to "/rooms/#{@room.id}"
+    end
     
-      def show
-        @room = Room.find(params[:id])
-        if Entry.where(:user_id => current_user.id, :room_id => @room.id).present?
-          @messages = @room.messages
-          @messages = @room.messages.includes(:user)
-          @message = Message.new
-          @entries = @room.entries
-          @participant_names = @entries.map { |entry| entry.user.name }.join('さんと ')
-        else
-          redirect_back(fallback_location: root_path)
-        end
+    def show
+      @room = Room.find(params[:id])
+      @entries = @room.entries.includes(:user)
+      if @entries.size == 2
+        @user1 = @entries.first.user
+        @user2 = @entries.second.user
+      else
+        # 万が一、エントリーが2人未満の場合の処理
+        redirect_to root_path, alert: "このルームには十分な参加者がいません。"
       end
-    end    
+      @messages = @room.messages.includes(:user)
+      @message = Message.new
+    end
+  end
